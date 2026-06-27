cbdfc8502c0384f7fe02141a056c72eeebcf167f
bondfrom00 <bondfrom00@gmail.com>
2026-06-27 20:17:14 +1200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
+<<<<<<< HEAD
 import path from "path";
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+=======
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+>>>>>>> first commit
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+<<<<<<< HEAD
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
@@ -18,3 +25,8 @@ export default defineConfig({
     },
   },
 });
+=======
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
+>>>>>>> first commit
