6ac7baf63541a47a1160f11bf1c83e33f310d3ea
bondfrom00 <bondfrom00@gmail.com>
2026-07-07 13:32:30 +1200

    adding git workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 import path from "path";
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
-=======
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
->>>>>>> first commit
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,7 +12,11 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
+<<<<<<< HEAD
   base: "/product-store-demo",
+=======
+  base: "/https://github.com/bondfrom00/product-store-demo/",
+>>>>>>> adding git workflow
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
